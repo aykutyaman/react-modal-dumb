72c8423d4965df4e256c4f9f837ab53072c8b715
Aykut Yaman <yaman.ayk@gmail.com>
2016-02-16 19:19:20 +0200

    prepublish.sh updated

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,8 +1,8 @@
 echo "> Start transpiling ES2015"
 echo ""
-babel --plugins "transform-runtime" lib --ignore __tests__ --out-dir ./dist
+node_modules/.bin/babel --plugins "transform-runtime" lib --ignore __tests__ --out-dir ./dist
 cd dist
-browserify --debug -t [ exposify --expose [ --react React ] ] ./window_bind.js > ./browser.js
+../node_modules/.bin/browserify --debug -t [ exposify --expose [ --react React ] ] ./window_bind.js > ./browser.js
 #cat ./browser.js | uglifyjs -c > ./browser.min.js
 echo ""
 echo "> Complete transpiling ES2015"
